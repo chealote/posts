6ad97976ae93d530475bb1cbcc285f9003458572
Alejandro Lazarte <chealote@gmail.com>
2024-10-06 16:51:16 -0300

    update web to use auth cookies properly

--- web/script.js
+++ web/script.js
@@ -4,11 +4,12 @@ const CONTENT_DIV = document.getElementById('content');
 async function fetchContent() {
   const response = await fetch(BASE_URL, {
     method: 'GET',
+    credentials: 'include',
   });
 
   return new Promise((resolve, reject) => {
     if (response.ok) {
-      return resolve(response.text);
+      return resolve(response.text());
     }
     reject('auth failed?');
   });
@@ -29,6 +30,7 @@ async function isLoggedIn() {
   // TODO: check if token in localStorage is valid
   const response = await fetch(`${BASE_URL}`, {
     method: 'GET',
+    credentials: 'include',
   });
 
   console.log(response);
@@ -44,11 +46,11 @@ function redirectIfInvalidSession() {
     if (ok) {
       loadContent();
     } else {
-      window.location.replace('login/login.html');
+      window.location.replace('signin/signin.html');
     }
   },
   err => {
-    window.location.replace('login/login.html');
+    window.location.replace('signin/signin.html');
   });
 }
 
