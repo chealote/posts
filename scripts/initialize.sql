a7c16db8286bb3805d99050b45d7e1fcc796a5fc
Alejandro Lazarte <chealote@gmail.com>
2024-11-10 16:15:18 -0300

    update sql scripts

--- scripts/initialize.sql
+++ scripts/initialize.sql
@@ -1,3 +1,5 @@
 CREATE TABLE IF NOT EXISTS sessions (username text primary key, token text, expires timestamp);
-CREATE TABLE IF NOT EXISTS users (username text primary key, password text, salt text);
+CREATE TABLE IF NOT EXISTS users (username text primary key, password text, salt text, roles text);
 CREATE TABLE IF NOT EXISTS posts (id text primary key, title text, post text, createdAt timestamp, createdBy username);
+CREATE TABLE IF NOT EXISTS availableRoles (roles text primary key);
+INSERT INTO availableRoles values ('read'),('create');
--- scripts/register-user.sql
+++ scripts/register-user.sql
@@ -1,1 +1,1 @@
-INSERT INTO users VALUES ($1, $2, $3)
+INSERT INTO users VALUES ($1, $2, $3, $4)
